5404ff82193820fae143b6256651d8d7b26f45fa
Valentin Orban <orban.valentin@gmail.com>
2018-09-02 16:20:37 +0200

    added fullscreen

--- src/Application.java
+++ src/Application.java
@@ -1,4 +1,5 @@
 import java.awt.Dimension;
+import java.awt.Toolkit;
 
 import javax.swing.JFrame;
 
@@ -9,19 +10,25 @@ public class Application {
 
 	public static void main(String[] args) {
 		
-		int scale = 5;
-		Dimension gameSize = new Dimension(300, 180);
-		Dimension windowSize = new Dimension(gameSize.width * scale, gameSize.height * scale);
-		
-		Game game = new Game(gameSize, scale);
-		RenderingPanel panel = new RenderingPanel(windowSize, game);
-		
 		JFrame window = new JFrame();
 		window.setTitle("GMTK Game Jam 2018");
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.setResizable(false);
+		window.setExtendedState(JFrame.MAXIMIZED_BOTH);
+		window.setUndecorated(true);
+		window.setAlwaysOnTop(false);
+		
+		int scale = 5;
+		Dimension windowSize = Toolkit.getDefaultToolkit().getScreenSize();
+		System.err.println(windowSize.width + "  " + windowSize.height);
+		Dimension gameSize = new Dimension(windowSize.width / scale, windowSize.height / scale);
+		
+		Game game = new Game(gameSize, scale);
+		RenderingPanel panel = new RenderingPanel(windowSize, game);
+		
+		
 		window.setContentPane(panel);
-		window.pack();
+		//window.pack();
 		window.setLocationRelativeTo(null);
 		window.setVisible(true);
 		
